b2f588b036c42558e6ba47bf75fe5ee11321956a
Jesus Anaya <jesus.anaya.dev@gmail.com>
2018-10-28 01:35:34 -0700

    Update deployment files.

---
+++ config/deployment_settings-sample.py
@@ -0,0 +1,24 @@
+from .settings import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '*@j$ybt^n#)%hu^us^ea@!2@3!@%^e^qj-h=(-4m_r%nj&4he5'
+
+ALLOWED_HOSTS = ['domain-example.com']
+
+DEBUG = False
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media_root")
---
+++ scripts/bash/install_ubuntu_services.sh
@@ -0,0 +1,12 @@
+apt install nginx
+apt install postgresql postgresql-contrib
+apt-get install build-essential checkinstall
+apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
+    libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+apt-get install python3-dev python3-setuptools python3-pip
+apt-get install libtiff5-dev libjpeg8-dev zlib1g-dev \
+    libfreetype6-dev liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev \
+    tcl8.6-dev tk8.6-dev python-tk
+apt-get install supervisor
+
+sudo pip3 install virtualenv
